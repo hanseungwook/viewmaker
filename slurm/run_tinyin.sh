82345ec45b7f472d62b64cb804b3c57d0fd43e70
Han <BNSSsgwh@dcsfen02.ccni.rpi.edu>
2022-05-10 16:03:35 -0400

    Fixed script for slurm update

--- slurm/run_tinyin.sh
+++ slurm/run_tinyin.sh
@@ -4,9 +4,8 @@
 #SBATCH -J vm_tinyin
 #SBATCH -o vm_tinyin_%j.out
 #SBATCH -e vm_tinyin_%j.err
-#SBATCH --nodes=1
 #SBATCH --gres=gpu:6
-#SBATCH --ntasks-per-node=6
+#SBATCH --cpus-per-gpu=24
 #SBATCH --mem=0
 #SBATCH --time=0-06:00:00
 #SBATCH --signal=SIGUSR1@90
