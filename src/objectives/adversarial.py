7cfa7cb21599b7adcf2d9ee733bc0f6b8d570962
Seungwook Han <sh3264@coumbia.edu>
2022-05-14 18:06:23 -0400

    Implemented supcon implementation of simclr

--- src/objectives/adversarial.py
+++ src/objectives/adversarial.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 
 from src.utils.utils import l2_normalize
-from src.objectives.simclr import SimCLRObjective
+from src.objectives.simclr import SimCLRObjective, SimCLRSupConObjective
 from src.objectives.infonce import NoiseConstrastiveEstimation
 
 
@@ -43,6 +43,41 @@ class AdversarialSimCLRLoss(object):
         view_maker_loss = -encoder_loss * self.view_maker_loss_weight
         return encoder_loss, view_maker_loss
 
+class AdversarialSimCLRSupConLoss(object):
+
+    def __init__(
+        self,
+        embs1,
+        embs2,
+        t=0.07,
+        view_maker_loss_weight=1.0,
+        **kwargs
+    ):
+        '''Adversarial version of SimCLR loss.
+        
+        Args:
+            embs1: embeddings of the first views of the inputs
+            embs1: embeddings of the second views of the inputs
+            t: temperature
+            view_maker_loss_weight: how much to weight the view_maker loss vs the encoder loss
+        '''
+        self.embs1 = embs1
+        self.embs2 = embs2
+        self.t = t
+        self.view_maker_loss_weight = view_maker_loss_weight
+
+        self.normalize_embeddings()
+
+    def normalize_embeddings(self):
+        self.embs1 = l2_normalize(self.embs1)
+        self.embs2 = l2_normalize(self.embs2)
+
+    def get_loss(self):
+        '''Return scalar encoder and view-maker losses for the batch'''
+        simclr_loss = SimCLRSupConObjective(self.embs1, self.embs2, self.t)
+        encoder_loss = simclr_loss.get_loss()
+        view_maker_loss = -encoder_loss * self.view_maker_loss_weight
+        return encoder_loss, view_maker_loss
 
 class AdversarialNCELoss(object):
 
--- src/objectives/simclr.py
+++ src/objectives/simclr.py
@@ -27,6 +27,57 @@ class SimCLRObjective(torch.nn.Module):
         loss = -torch.mean(witness_score / self.t - witness_norm)
         return loss
 
+class SimCLRSupConObjective(torch.nn.Module):
+
+    def __init__(self, outputs1, outputs2, t, base_t=0.07, push_only=False):
+        super().__init__()
+        self.outputs1 = l2_normalize(outputs1, dim=1)
+        self.outputs2 = l2_normalize(outputs2, dim=1)
+        self.t = t
+        self.base_t = base_t
+        self.push_only = push_only
+
+    def get_loss(self):
+        batch_size = self.outputs1.shape[0]
+
+        contrast_count = 2
+        contrast_feature = torch.cat([self.outputs1, self.outputs2], dim=0)
+        anchor_feature = contrast_feature
+        anchor_count = contrast_count
+
+        # compute logits
+        anchor_dot_contrast = torch.div(
+            torch.matmul(anchor_feature, contrast_feature.T),
+            self.t)
+        # for numerical stability
+        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+        logits = anchor_dot_contrast - logits_max.detach()
+
+        # tile mask
+        mask = torch.eye(batch_size, dtype=torch.float32).to(contrast_feature.device)
+        mask = mask.repeat(anchor_count, contrast_count)
+        # mask-out self-contrast cases
+        logits_mask = torch.scatter(
+            torch.ones_like(mask),
+            1,
+            torch.arange(batch_size * anchor_count).view(-1, 1).to(contrast_feature.device),
+            0
+        )
+        mask = mask * logits_mask
+
+        # compute log_prob
+        exp_logits = torch.exp(logits) * logits_mask
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+        # compute mean of log-likelihood over positive
+        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+
+        # loss
+        loss = - (self.t / self.base_t) * mean_log_prob_pos
+        loss = loss.view(anchor_count, batch_size).mean()
+
+        return loss
+
 # class SimCLRObjective3View(torch.nn.Module):
 
 #     def __init__(self, outputs1, outputs2, t, push_only=False):
